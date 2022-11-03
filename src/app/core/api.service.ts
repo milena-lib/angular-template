0ad8622d47ffea422b0f6ef21de4ed846d4323cf
milena-lib <milena.liberman@gmail.com>
2022-11-03 22:30:28 +0200

    fix import

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Product } from '../shared/models/product.model';
+import { Product } from 'src/app/shared/models/product.model';
 
 @Injectable({
   providedIn: 'root'
